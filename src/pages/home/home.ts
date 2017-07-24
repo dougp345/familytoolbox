f3d6347ed19a35800010088594f5452d6ed8a54d
Doug <Doug@Ender.local>
2017-07-24 10:23:53 -0500

    Updated Calendar.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,16 +6,50 @@ import * as moment from 'moment';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
   currentMonthYear: string;
+  gridPosition: object = {};
+  dayContent: object = {};
 
   constructor(public navCtrl: NavController) {
-    this.goCurrentMonth();
+    this.goMonth(moment().format('MM'), moment().format('YYYY'));
   }
 
-  goCurrentMonth() {
-    let now = moment();
-    this.currentMonthYear = now.format('MMMM YYYY');
+  goMonth(month: string, year: string) {
+    let x: number;
+    let y: number;
+    let currentDay: number = 1;
+    this.currentMonthYear = moment(month, 'MM').format('MMMM') + " " + year;
+    let daysInMonth = moment(year + "-" + month, "YYYY-MM").daysInMonth();
+    let firstDayOfMonth = moment(year + "-" + month + "-01", "YYYY-MM-DD").format('d');
+
+    for (x = 0; x < 7; x++) {
+      this.gridPosition[x] = {};
+      this.dayContent[x] = {};
+      for (y = 0; y < 7; y++) {
+        this.gridPosition[x][y] = "";
+        this.dayContent[x][y] = [];
+      }
+    }
+
+    x = Number(firstDayOfMonth);
+    y = 0;
+
+    while (currentDay <= daysInMonth) {
+      // Set day of the month
+      this.gridPosition[x][y] = currentDay;
+      // Set day content
+      // ???
+      currentDay += 1;
+      x = ((x + 1) % 7);
+      if (x == 0) {
+        y += 1;
+      }
+    }
+
+    this.dayContent[6][0] = [{content: "Some", color: "danger"}];
+    this.dayContent[4][2] = [{content: "Wow", color: "primary"}, {content: "Dance", color: "secondary"}];
   }
 
 }
